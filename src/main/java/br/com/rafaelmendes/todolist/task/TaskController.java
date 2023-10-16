92cc543eab9e29e5d7b4075f0842d8a37c0f5b1e
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-10-16 19:11:12 -0300

    Implementando o metodo get e put na classe TaskController

--- src/main/java/br/com/rafaelmendes/todolist/task/TaskController.java
+++ src/main/java/br/com/rafaelmendes/todolist/task/TaskController.java
@@ -1,17 +1,21 @@
 package br.com.rafaelmendes.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rafaelmendes.todolist.utils.Ultils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -29,14 +33,45 @@ public class TaskController {
         var currentDate = LocalDateTime.now();
         // 10//11/2023 - current
         // 10/10/2023 - statAt
-        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ) {
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("A data de inicio / data de termino deve ser maior que a data atual");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio deve ser menor que a data de término");
         }
 
         var task = this.taskRepository.save(taskModel);
 
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var idUser = request.getAttribute("idUser");
+
+        var task = this.taskRepository.findById(id).orElse(null);
+        if(task == null){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Tarefa não encontrada!");
+        }
+      
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuario nao tem peermissão para alterar esta tarefa");
+        }
+
+        Ultils.copyNonNullProperties(taskModel, task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
     }
 
 }
--- src/main/java/br/com/rafaelmendes/todolist/task/TaskRepository.java
+++ src/main/java/br/com/rafaelmendes/todolist/task/TaskRepository.java
@@ -3,7 +3,11 @@ package br.com.rafaelmendes.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+    //TaskModel findByIdAndIdUser(UUID id, UUID idUser);
     
 }
